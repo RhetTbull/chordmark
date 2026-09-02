42a3c53a013d593b878eaae2c6176ea6af8f7c4a
Rhet Turnbull <rturnbull@gmail.com>
2026-09-02 07:35:17 -0400

    Initial commit

---
+++ Chordmark/Chordmark/ChordmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChordmarkApp.swift
+//  Chordmark
+//
+//  Created by Rhet Turnbull on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ChordmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chordmark/ChordmarkTests/ChordmarkTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChordmarkTests.swift
+//  ChordmarkTests
+//
+//  Created by Rhet Turnbull on 9/2/26.
+//
+
+import Testing
+@testable import Chordmark
+
+struct ChordmarkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
